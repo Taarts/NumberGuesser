add6e347251af9b6fca266f93a7c1024eb7312b2
Tricia Amheiser <tricia@taarts.net>
2022-05-04 22:51:14 -0400

    User interaction Y/N & H/L working

--- Program.cs
+++ Program.cs
@@ -2,11 +2,84 @@
 
 namespace NumberGuesser
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void DisplayGreeting()
+        {
+            //  #Greeting
+            Console.WriteLine("Hello, I'm great at guessing numbers,");
+            Console.WriteLine();
+            // #request
+            Console.WriteLine("Give me a number between 1 - 1024,");
+            Console.WriteLine("Don't tell me the number, Let me guess!");
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            // 
+            var min = 1;
+            var max = 1024;
+            var mid = (min + max) / 2;
+            DisplayGreeting();
+            // GuessNumber(min, max);
+
+
+
+            // #GuessNumber
+            Console.WriteLine();
+            Console.WriteLine($"Is your number {mid}?");
+            Console.WriteLine("Type 'Y' if true, 'N' if false. ");
+            var UserSays = Console.ReadLine();
+
+            // If N:
+            if (UserSays == "N")
+            {
+                // #Filter
+                Console.WriteLine();
+                Console.WriteLine("Is your number HIGHER or LOWER? ");
+                Console.WriteLine("Type 'H' if HIGHER, 'L' if LOWER.  ");
+                var answer = Console.ReadLine();
+                //      IF H
+                if (answer == "H")
+                {
+                    var guess = (mid + max) / 2;
+                    Console.WriteLine();
+                    Console.WriteLine($"Is your number {guess}? ");
+
+
+
+                    // the NewMidPoint is now #Guess
+                    // GoTO #Guess
+
+                }
+                //         else if (UserSays == L)
+                //         {
+                //             Guess = max;
+                //               var NewGuess = (min / 2 );
+                //               Console.WriteLine($"Is your number {NewGuess}? ")
+
+            }
+            // - Is your number Higher or Lower
+            // - Type "H" or "L"
+            //  
+            //  Else if L
+            // #GUESS = HighNum
+            // BottomMarker = LowNum
+            // MidPoint is now the #Guess
+            // GoTO #Guess
+            // Else
+            // If Y:
+            // - Excellent! It's great to be correct!
+            // - Wanna play again?
+            // If Y
+            // GOTO #Request
+            // If N
+            // - OK thanks for playing, Goodbye!
+
+        }
+
+
     }
-  }
 }
+
--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # NumberGuesser
+
+#Presentation
+Min = 1
+Max = 1024
+
+- Hello I'm great at guessing numbers
+  #request
+- Give me a number between 1 - 1024
+  min = 1
+  max = 1024
+  first guess
+- Don't tell me the number, Let me guess!
+  #Guess
+- Is your number {Guess}?
+  #Confirm
+  Did they type something else?
+  #Y_N
+  Else #Confirm
+  GOTO #END
+  If N:
+  #Filter
+
+  - Is your number Higher or Lower
+  - Type "H" or "L"
+    **Did they type something else?
+    **GOTO #END
+    IF H
+    #Guess = min;
+    NewGuess = (min / 2) + min
+
+    - Is your number {NewGuess}?
+
+    Else if L
+    #GUESS = Max
+    NewGuess = max / 2
+
+    - Is your number NewGuess?
+      GoTO #Guess
+      Else
+      If Y:
+
+- Excellent! It's great to be correct!
+- Wanna play again?
+  If Y
+  GOTO #Request
+  If N
+- OK thanks for playing, Goodbye!
+  #END
+- That's not what I asked for, I'm not playing anymore.
