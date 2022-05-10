87198be75ca06bb5c985320babe7741440feae33
Tricia Amheiser <tricia@taarts.net>
2022-05-10 17:06:16 -0400

    Methods are all working - Explorer mode

--- Program.cs
+++ Program.cs
@@ -4,92 +4,72 @@ namespace NumberGuesser
 {
     class Program
     {
+        // greeting Method
         static void DisplayGreeting()
         {
             //  #Greeting
+            Console.WriteLine();
             Console.WriteLine("Hello, I'm great at guessing numbers,");
             Console.WriteLine();
             // #request
-            Console.WriteLine("Give me a number between 1 - 1024,");
-            Console.WriteLine("Don't tell me the number, Let me guess!");
+            Console.WriteLine("Think of a number between 1 - 1024,");
+            Console.WriteLine("don't tell me the number, let me guess!");
             Console.WriteLine();
+        }
+        // Guess method (loop)
+        static void GuessNumber(int low, int high)
+        {
+            var mid = (low + high) / 2;
+            Console.WriteLine();
+            Console.WriteLine($"Is your number {mid}? Type 'Y' for a corrrect guess, 'L' for lower, 'H' for higher.  ");
+            var answer = Console.ReadLine();
+
+            if (answer == "Y")
+            {
+                Console.WriteLine("Wow, It's great to be a clever computer!");
+            }
+            else if (answer == "H")
+            {
+                var NewMin = mid;
+                // Console.WriteLine("mid " + mid);
+                GuessNumber(NewMin, high);
+            }
+            else if (answer == "L")
+            {
+                var NewMax = mid;
+                // Console.WriteLine("mid " + mid);
+                GuessNumber(low, NewMax);
+            }
+            else
+            {
+                Console.WriteLine("error checking");
+            }
+        }
+        static string PromptForAnswer(string prompt)
+        {
+            // Use the argument, get Y/N - H/L
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+            return userInput;
         }
 
         static void Main(string[] args)
         {
-            // 
             var min = 1;
             var max = 1024;
-            var mid = (min + max) / 2;
+
+            //start
+            //greeting
             DisplayGreeting();
-            // GuessNumber(min, max);
+
+            // Guess
+            GuessNumber(min, max);
 
 
-
-            // #GuessNumber
-            Console.WriteLine();
-            Console.WriteLine($"Is your number {mid}?");
-            Console.WriteLine("Type 'Y' if true, 'N' if false. ");
-            var UserSays = Console.ReadLine();
-
-            // If N:
-            // while (UserSays != "Y")
-            if (UserSays == "N")
-
-            {   // #Filter
-                // - Is your number Higher or Lower
-                // - Type "H" or "L"
-                Console.WriteLine();
-                Console.WriteLine("Is your number HIGHER or LOWER? ");
-                Console.WriteLine("Type 'H' if HIGHER, 'L' if LOWER.  ");
-                var answer = Console.ReadLine();
-
-
-                switch (answer)
-                //      IF H
-                {
-                    case "H":
-                        var hGuess = (mid + max) / 2;
-                        Console.WriteLine();
-                        Console.WriteLine($"Is your number {hGuess}? ");
-                        break;
-
-                    //  if L
-                    case "L":
-
-                        var lGuess = (mid + min) / 2;
-                        Console.WriteLine();
-                        Console.WriteLine($"Is your number {lGuess}? ");
-                        break;
-
-
-                    // #GUESS = HighNum
-                    // BottomMarker = LowNum
-                    // MidPoint is now the #Guess
-                    // GoTO #Guess
-                    // Else
-                    case "Y":
-
-                        Console.WriteLine();
-                        Console.WriteLine("Excellent! It's great to be correct!");
-                        Console.WriteLine("Wanna play again?");
-                        break;
-
-                    default:
-                        Console.WriteLine();
-                        Console.WriteLine("That's not what I asked for, I'm not playing anymore");
-                        Console.WriteLine();
-                        break;
-                }
-
-                // If Y
-                // GOTO #Request
-                // If N
-                // - OK thanks for playing, Goodbye!
-
-            }
 
         }
+
     }
 }
+
 
